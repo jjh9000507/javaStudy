ffd0367cecfa038f1a778a5a56437adfa3aad7b5
jjh9000507 <jjh9000507@hanmail.net>
2021-06-15 15:06:03 +0900

    6/15 자바 공부

---
+++ chap02/src/chap02/exam02/VariableXwcahngeExample.java
@@ -0,0 +1,17 @@
+package chap02.exam02;
+
+public class VariableXwcahngeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 5;
+		System.out.println("x:" + x + ", y:" + y);
+		
+		int temp = x;
+		x = y;
+		y = temp;
+		System.out.println("x:" + x + ", y:" + y);
+	}
+
+}
